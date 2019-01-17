fba8fb2bc531ae4ffcda56d93bafd0f889dbb444
Joshua <joshua90123@gmail.com>
2019-01-17 12:37:37 -0700

    Line Finding

--- Lab1_4/Lab1_4.ino
+++ Lab1_4/Lab1_4.ino
@@ -1,17 +1,31 @@
+#include <AFMotor.h>
 
 const uint8_t LASER_PIN = 14;
-const uint8_t LED_PIN = 15;
+// const uint8_t LED_PIN = 15;
 const uint8_t OPTO_PIN = A0;
+
+AF_DCMotor motorR(1);
+AF_DCMotor motorL(2);
+
+const uint8_t LEFT_SPEED = 80;
+const uint8_t RIGHT_SPEED = 60;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
 
   pinMode(LASER_PIN, OUTPUT);
-  pinMode(LED_PIN, OUTPUT);
+  // pinMode(LED_PIN, OUTPUT);
   pinMode(OPTO_PIN, INPUT);
 
   digitalWrite(LASER_PIN, HIGH);
+
+    motorR.setSpeed(200);
+    motorL.setSpeed(200);
+    motorR.run(FORWARD);
+    motorL.run(FORWARD);
+
+    delay(50);
 
 }
 
@@ -20,12 +34,29 @@ void loop() {
 
   int32_t optoValue = analogRead(OPTO_PIN);
 
-  // Todo: Calibrate the Optosensor to detect tape and floor
-  // Somewhere between 990 - 1010 is tape
-  if(optoValue > 1010) {
-    digitalWrite(LED_PIN, HIGH);
-  } else {
-    digitalWrite(LED_PIN, LOW);
+  motorR.setSpeed(RIGHT_SPEED);
+  motorL.setSpeed(LEFT_SPEED);
+
+
+    motorR.setSpeed(RIGHT_SPEED);
+    motorL.setSpeed(LEFT_SPEED);
+    
+    motorR.run(FORWARD);
+    delay(50);
+    motorL.run(FORWARD);
+  
+  if(optoValue > 1018) {
+    // digitalWrite(LED_PIN, HIGH);
+    motorR.run(RELEASE);
+    motorL.run(RELEASE);
+    motorR.run(BACKWARD);
+    motorL.run(BACKWARD);
+
+    delay(600);
+    motorR.run(RELEASE);
+    motorL.run(RELEASE);
+
+    while(1);
   }
 
   Serial.println(optoValue);
